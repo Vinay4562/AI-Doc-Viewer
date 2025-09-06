f50366538a9abb0931c7ce26233a98fcf2aba5dc
Vinay Kumar <manikantivinaykumar@yahoo.com>
2025-09-06 22:41:12 +0530

    Force redeploy .NET API to pick up new configuration

--- dotnet-api/Program.cs
+++ dotnet-api/Program.cs
@@ -7,6 +7,8 @@ using Amazon.S3;
 using Amazon.S3.Model;
 using Npgsql;
 
+// Force redeploy to pick up new configuration
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Basic services, DB and HTTP client for Python service
